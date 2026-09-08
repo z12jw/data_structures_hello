7f457e6e0ed6daff66850cb3e0f88f6a09c54e2c
z12jw <3504564121@qq.com>
2026-09-08 20:23:23 +0800

    直接用python中现成队列类

---
+++ python/chapter_stack_and_queue/queue.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+# 初始化队列
+# 在 Python 中，我们一般将双向队列类 deque 当作队列使用
+# 虽然 queue.Queue() 是纯正的队列类，但不太好用，因此不推荐
+
+que : deque[int] = deque()
+
+#元素入队
+que.append(1)
+que.append(2)
+
+#访问队首元素
+front: int = que[0]
+
+#元素出队   
+pop: int = que.popleft()
+
+#获取队列长度
+size : int = len(que)
+
+#判断队列是否为空
+is_empty: bool = len(que) == 0
